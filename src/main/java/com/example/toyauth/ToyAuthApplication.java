0a61167fe291841b4d763aca12f5030e08eb61be
Minchae0322 <jmcabc0322@gmail.com>
2025-01-12 22:56:12 +0900

    setting: 초기 설정

---
+++ src/main/java/com/example/toyauth/ToyAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.toyauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/toyauth/ToyAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toyauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
