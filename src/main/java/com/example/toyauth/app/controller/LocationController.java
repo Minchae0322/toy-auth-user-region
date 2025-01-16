6cdee4904af7c9ea82ed895e88c6726f5b067cd7
Minchae0322 <jmcabc0322@gmail.com>
2025-01-16 22:29:49 +0900

    feat: auth component

---
+++ src/main/java/com/example/toyauth/app/controller/LocationController.java
@@ -0,0 +1,28 @@
+package com.example.toyauth.app.controller;
+
+import com.example.toyauth.app.dto.KakaoSearchLocationResponse;
+import com.example.toyauth.app.service.LocationService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "LocationController", description = "위치 서비스")
+@RestController(value = "/location")
+@RequiredArgsConstructor
+public class LocationController {
+
+    private final LocationService locationService;
+
+    @GetMapping("")
+    @Operation(summary = "카카오 위치 API",
+            tags = "LocationController")
+    public ResponseEntity<KakaoSearchLocationResponse> getLocation(@RequestParam @Valid String place) {
+        return locationService.getLocationByKeyword(place);
+    }
+
+}
