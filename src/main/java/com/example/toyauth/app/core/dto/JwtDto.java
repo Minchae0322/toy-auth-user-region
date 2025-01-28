71e570fea47f1f851fa7b326b23ada2628743fca
Minchae0322 <jmcabc0322@gmail.com>
2025-01-28 21:25:32 +0900

    fix: jwtDto missing builder

--- src/main/java/com/example/toyauth/app/core/dto/JwtDto.java
+++ src/main/java/com/example/toyauth/app/core/dto/JwtDto.java
@@ -1,8 +1,10 @@
 package com.example.toyauth.app.core.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class JwtDto {
     private String grantType;
     private String accessToken;
