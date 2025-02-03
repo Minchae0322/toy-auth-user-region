bb351d336d51b43a99688a1987e749a55eff6883
Minchae0322 <jmcabc0322@gmail.com>
2025-02-04 00:30:22 +0900

    setting: version down

--- src/main/java/com/example/toyauth/app/auth/domain/MyUserDetails.java
+++ src/main/java/com/example/toyauth/app/auth/domain/MyUserDetails.java
@@ -60,4 +60,24 @@ public class MyUserDetails implements UserDetails, OAuth2User {
     public String getUsername() {
         return user.getUsername();
     }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
