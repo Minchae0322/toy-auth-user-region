d39bc45255cf71e547a8b49c7009ec6f8a523e92
Minchae0322 <jmcabc0322@gmail.com>
2025-02-03 23:29:38 +0900

    test: jenkins test

--- src/test/java/com/example/toyauth/ToyAuthApplicationTests.java
+++ src/test/java/com/example/toyauth/ToyAuthApplicationTests.java
@@ -3,11 +3,4 @@ package com.example.toyauth;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ToyAuthApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
-
-}
