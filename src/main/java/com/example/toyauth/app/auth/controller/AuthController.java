d1cea8506ee6a9ec93a322a0b9127e9206491bb3
Minchae0322 <jmcabc0322@gmail.com>
2025-02-10 21:18:21 +0900

    feat: common user login api

--- src/main/java/com/example/toyauth/app/auth/controller/AuthController.java
+++ src/main/java/com/example/toyauth/app/auth/controller/AuthController.java
@@ -2,16 +2,19 @@ package com.example.toyauth.app.auth.controller;
 
 import com.example.toyauth.app.auth.domain.dto.LoginRequestDto;
 import com.example.toyauth.app.auth.service.AuthService;
+import com.example.toyauth.app.common.handler.LoginSuccessHandler;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @Tag(name = "AuthController", description = "사용자 로그인")
 @RestController
@@ -19,13 +22,18 @@ import org.springframework.web.bind.annotation.RestController;
 @SecurityRequirement(name = "oauth2")
 @RequestMapping
 public class AuthController {
-    private final AuthService authService;
 
-    @GetMapping("/login")
+    private final AuthService authService;
+    private final LoginSuccessHandler loginSuccessHandler;
+
+    @PostMapping("/login")
     @Operation(summary = "사용자 로그인",
             tags = "AuthController")
-    public ResponseEntity<?> login(@RequestBody @Valid LoginRequestDto loginRequestDto) {
-        return ResponseEntity.ok(authService.login(loginRequestDto));
+    public void login(@RequestBody @Valid LoginRequestDto loginRequestDto, HttpServletRequest request,
+                      HttpServletResponse response) throws IOException {
+        Authentication authentication = authService.login(loginRequestDto);
+
+        loginSuccessHandler.onAuthenticationSuccess(request, response, authentication);
     }
 
     @GetMapping("/oauth2/authorization/github")
--- src/main/java/com/example/toyauth/app/auth/service/AuthService.java
+++ src/main/java/com/example/toyauth/app/auth/service/AuthService.java
@@ -2,14 +2,29 @@ package com.example.toyauth.app.auth.service;
 
 import com.example.toyauth.app.location.domain.dto.KakaoSearchLocationResponse;
 import com.example.toyauth.app.auth.domain.dto.LoginRequestDto;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class AuthService {
-    public ResponseEntity<KakaoSearchLocationResponse> login(LoginRequestDto loginRequestDto) {
-        return null;
+
+    private final AuthenticationManager authenticationManager;
+
+    public Authentication login(LoginRequestDto loginRequestDto) {
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginRequestDto.email(), loginRequestDto.password());
+
+        Authentication authentication = authenticationManager.authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return authentication;
     }
 }
